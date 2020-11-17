653e9b91881343444c46fd74232e4acedef3c5f7
CherryHoneyBadger <48109241+CherryHoneyBadger@users.noreply.github.com>
2020-11-17 15:42:00 +0900

    update

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,25 @@
 window.onload = init;
 
 function init() {
+
+    var view = new ol.View({
+        center: [0, 0],
+        zoom: 8,
+        extent: [14269650.441621488, 4311713.3414360015, 14399732.3423705, 4450133.8576256605],
+        constrainOnlyCenter: true
+    })
+
     var map = new ol.Map({
-        view: new ol.View({
-            center: [0, 0],
-            zoom: 2
-        }),
         layers: [
             new ol.layer.Tile({
                 source: new ol.source.OSM()
             })
         ],
-        target: 'js-map'
+        target: 'map',
+        view: view
+    })
+
+    map.on('click', function(e) {
+        console.log(e.coordinate);
     })
 }
