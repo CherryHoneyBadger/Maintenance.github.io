54cb3824a331190787d2a4705452ee1edffd29e1
CherryHoneyBadger <48109241+CherryHoneyBadger@users.noreply.github.com>
2020-11-18 15:54:29 +0900

    2020-11-18 Update v0.3

--- JS/main.js
+++ JS/main.js
@@ -3,13 +3,19 @@ window.onload = init;
 function init() {
 
     var view = new ol.View({
-        center: [0, 0],
+        //지도의 제약범위 설정, 지도 중앙 설정, 줌 거리 설정, 
+        center: [14329735.536780592, 4378371.922890938],
         zoom: 8,
-        extent: [14269650.441621488, 4311713.3414360015, 14399732.3423705, 4450133.8576256605],
-        constrainOnlyCenter: true
+        //3D 지도를 볼경우 제약 범위를 주석처리 해야함 
+        //제약 범위를 성정해 놓으면 제약 범위가 작동하지 않는다
+        //extent: [14269650.441621488, 4311713.3414360015, 14399732.3423705, 4450133.8576256605],
+        //constrainOnlyCenter: true,
+        //minZoom: 12,
+        //maxZoom: 20
     })
 
     var map = new ol.Map({
+        //지도 표시 OSM(OpenStreetMap)
         layers: [
             new ol.layer.Tile({
                 source: new ol.source.OSM()
@@ -19,7 +25,21 @@ function init() {
         view: view
     })
 
+    //2D 지도를 3D 지도로 바꿔주는 코드
+    var ol3d = new olcs.OLCesium({
+        map: map
+    })
+
+    var scene = ol3d.getCesiumScene();
+    scene.terrainProvider = Cesium.createWorldTerrain();
+    //ol3d.setEnabled 안의 boolean 값이 true일 경우 3d 지도 표시 false 일 경우 2d 지도를 표시 한다
+    ol3d.setEnabled(false);
+
+
+    // 클릭시 브라우저 콘솔에 좌표 표시
+    /*
     map.on('click', function(e) {
         console.log(e.coordinate);
     })
+    */
 }
--- README.md
+++ README.md
@@ -5,16 +5,25 @@
 
 # -- Update Note --
 
+## --- v0.3 ---
+
+### == 2020-11-18 업데이트 내용 ==
+
+    OpenLayers - OL-Cesium API를 사용하여 기존의 2D 지도를 3D로 만들었습니다.
+    3D 지도를 사용지 기존의 제약 범위 설정을 주석 처리 하였습니다. 왜냐하면
+    3D 지도를 볼 때 지도가 제약 범위로 인해서 오작동 할 수 있습니다.
+
 ## --- v0.2 ---
 
 ### == 2020-11-17 업데이트 내용 ==
+
     OpenLayers - Constrained Extent를 사용하여 제약 범위를 설정 하였습니다,
     이제 사용자는 개발자가 지정한 위치에서만 움직일수 있고 그외 위치로는 
     이동할 수 없습니다.
 
 
 # -- API -- 
-    * Openlayers
-    * Ol-Cesium
-    * CesiumJS
+    * Openlayers : https://openlayers.org/
+    * Ol-Cesium  : https://openlayers.org/ol-cesium/
+    * CesiumJS   : https://cesium.com/cesiumjs/
 
