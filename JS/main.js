abfe8b21277b30565168a065c30187c7aae91720
CherryHoneyBadger <48109241+CherryHoneyBadger@users.noreply.github.com>
2020-12-17 16:18:52 +0900

    Re Update v0.7

--- JS/main.js
+++ JS/main.js
@@ -53,6 +53,11 @@ var view = new ol.View({
 
 
 var map = new ol.Map({
+    controls: new ol.control.defaults().extend([
+        new ol.control.FullScreen({
+            source: 'fullscreen',
+        })
+    ]),
     //지도 표시 OSM(OpenStreetMap)
     layers: [
         new ol.layer.Tile({
@@ -78,7 +83,7 @@ var ol3d = new olcs.OLCesium({
 var scene = ol3d.getCesiumScene();
 scene.terrainProvider = Cesium.createWorldTerrain();
 //ol3d.setEnabled 안의 boolean 값이 true일 경우 3d 지도 표시 false 일 경우 2d 지도를 표시 한다
-ol3d.setEnabled(true);
+ol3d.setEnabled(false);
 
 map.on('singleclick', function(evt) {
     var coordinate = evt.coordinate;
@@ -91,132 +96,131 @@ map.on('singleclick', function(evt) {
 //View Animation Functions
 
 function bounce(t) {
-  var s = 7.5625;
-  var p = 2.75;
-  var l;
-  if (t < 1 / p) {
-    l = s * t * t;
-  } else {
-    if (t < 2 / p) {
-      t -= 1.5 / p;
-      l = s * t * t + 0.75;
+    var s = 7.5625;
+    var p = 2.75;
+    var l;
+    if (t < 1 / p) {
+        l = s * t * t;
     } else {
-      if (t < 2.5 / p) {
-        t -= 2.25 / p;
-        l = s * t * t + 0.9375;
-      } else {
-        t -= 2.625 / p;
-        l = s * t * t + 0.984375;
-      }
+        if (t < 2 / p) {
+            t -= 1.5 / p;
+            l = s * t * t + 0.75;
+        } else {
+            if (t < 2.5 / p) {
+                t -= 2.25 / p;
+                l = s * t * t + 0.9375;
+            } else {
+                t -= 2.625 / p;
+                l = s * t * t + 0.984375;
+            }
+        }
     }
-  }
-  return l;
+    return l;
 }
 
 // An elastic easing method (from https://github.com/DmitryBaranovskiy/raphael).
 function elastic(t) {
-  return (
-    Math.pow(2, -10 * t) * Math.sin(((t - 0.075) * (2 * Math.PI)) / 0.3) + 1
-  );
+    return (
+        Math.pow(2, -10 * t) * Math.sin(((t - 0.075) * (2 * Math.PI)) / 0.3) + 1
+    );
 }
 
 function onClick(id, callback) {
-  document.getElementById(id).addEventListener('click', callback);
+    document.getElementById(id).addEventListener('click', callback);
 }
 
-onClick('rotate-left', function () {
-  view.animate({
-    rotation: view.getRotation() + Math.PI / 2,
-  });
+onClick('rotate-left', function() {
+    view.animate({
+        rotation: view.getRotation() + Math.PI / 2,
+    });
 });
 
-onClick('rotate-right', function () {
-  view.animate({
-    rotation: view.getRotation() - Math.PI / 2,
-  });
+onClick('rotate-right', function() {
+    view.animate({
+        rotation: view.getRotation() - Math.PI / 2,
+    });
 });
 
-onClick('pan-to-london', function () {
-  view.animate({
-    center: london,
-    duration: 2000,
-  });
+onClick('pan-to-london', function() {
+    view.animate({
+        center: london,
+        duration: 2000,
+    });
 });
 
-onClick('elastic-to-moscow', function () {
-  view.animate({
-    center: moscow,
-    duration: 2000,
-    easing: elastic,
-  });
+onClick('elastic-to-moscow', function() {
+    view.animate({
+        center: moscow,
+        duration: 2000,
+        easing: elastic,
+    });
 });
 
-onClick('bounce-to-istanbul', function () {
-  view.animate({
-    center: istanbul,
-    duration: 2000,
-    easing: bounce,
-  });
+onClick('bounce-to-istanbul', function() {
+    view.animate({
+        center: istanbul,
+        duration: 2000,
+        easing: bounce,
+    });
 });
 
 function flyTo(location, done) {
-  var duration = 2000;
-  var zoom = view.getZoom();
-  var parts = 2;
-  var called = false;
-  function callback(complete) {
-    --parts;
-    if (called) {
-      return;
+    var duration = 2000;
+    var zoom = view.getZoom();
+    var parts = 2;
+    var called = false;
+
+    function callback(complete) {
+        --parts;
+        if (called) {
+            return;
+        }
+        if (parts === 0 || !complete) {
+            called = true;
+            done(complete);
+        }
     }
-    if (parts === 0 || !complete) {
-      called = true;
-      done(complete);
-    }
-  }
-  view.animate(
-    {
-      center: location,
-      duration: duration,
-    },
-    callback
-  );
-  view.animate(
-    {
-      zoom: zoom - 1,
-      duration: duration / 2,
-    },
-    {
-      zoom: zoom,
-      duration: duration / 2,
-    },
-    callback
-  );
+    view.animate({
+            center: location,
+            duration: duration,
+        },
+        callback
+    );
+    view.animate({
+            zoom: zoom - 1,
+            duration: duration / 2,
+        }, {
+            zoom: zoom,
+            duration: duration / 2,
+        },
+        callback
+    );
 }
 
-onClick('fly-to-bern', function () {
-  flyTo(bern, function () {});
+onClick('fly-to-bern', function() {
+    flyTo(bern, function() {});
 });
 
 function tour() {
-  var locations = [london, bern, rome, moscow, istanbul];
-  var index = -1;
-  function next(more) {
-    if (more) {
-      ++index;
-      if (index < locations.length) {
-        var delay = index === 0 ? 0 : 750;
-        setTimeout(function () {
-          flyTo(locations[index], next);
-        }, delay);
-      } else {
-        alert('Tour complete');
-      }
-    } else {
-      alert('Tour cancelled');
+    var locations = [london, bern, rome, moscow, istanbul];
+    var index = -1;
+
+    function next(more) {
+        if (more) {
+            ++index;
+            if (index < locations.length) {
+                var delay = index === 0 ? 0 : 750;
+                setTimeout(function() {
+                    flyTo(locations[index], next);
+                }, delay);
+            } else {
+                alert('Tour complete');
+            }
+        } else {
+            alert('Tour cancelled');
+        }
     }
-  }
-  next(true);
+    next(true);
 }
 
 onClick('tour', tour);
